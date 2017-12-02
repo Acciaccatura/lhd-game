b3178786557696eb0cde46450e86c74b67c8c0c6
Acciaccatura <2899523@gmail.com>
2017-12-02 15:31:27 -0600

    fixed gravity

--- src/Game.java
+++ src/Game.java
@@ -51,7 +51,7 @@ public class Game {
 	}
 	
 	public void draw(Graphics g) {
-		if (++gravity >= 10) gravity = -5;
+		if (--gravity <= -15) gravity = 0;
 		g.fillRect(0, 0, Main.GAME_WIDTH, Main.GAME_HEIGHT);
 		bg.draw(g);
 		main_dude.draw(g);
