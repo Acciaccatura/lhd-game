b33988d304030b52fe7b28718e47333cdb22defb
Acciaccatura <2899523@gmail.com>
2017-12-02 12:24:16 -0600

    game loop?

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 public class Main {
 	
+	public static final int GAME_WIDTH = 600;
+	public static final int GAME_HEIGHT = 1000;
+	
+	public static JFrame frame;
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		frame = new JFrame("some java game lol");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
+		frame.pack();
+		frame.setVisible(true);
+		Game g = new Game();
+		g.start();
 	}
 	
 }
